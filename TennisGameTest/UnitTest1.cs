645853ed30665190e060edb3032098dc5b65110c
Chunhui <Chunhui831211@gmail.com>
2020-01-14 22:08:30 +0800

    test : Love All

---
+++ TennisGameTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TennisGameTDD;
+
+namespace TennisGameTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void LoveAll()
+        {
+            var target = new TennisGame();
+            var actual = target.score();
+            Assert.AreEqual("Love All", actual);
+        }
+    }
+}
