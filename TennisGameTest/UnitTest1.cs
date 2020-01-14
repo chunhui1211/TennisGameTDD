19922a82fb8e246cbf78b1286969e000b97b879f
Chunhui <Chunhui831211@gmail.com>
2020-01-14 22:29:43 +0800

    Withdraw target

--- TennisGameTest/UnitTest1.cs
+++ TennisGameTest/UnitTest1.cs
@@ -7,39 +7,41 @@ namespace TennisGameTest
     [TestClass]
     public class UnitTest1
     {
+        private TennisGame _target;
+        public UnitTest1()
+        {
+            _target = new TennisGame();
+        }
         [TestMethod]
         public void FortyLove()
         {
-            var target = new TennisGame();
-            target.HomePlayerScore();
-            target.HomePlayerScore();
-            target.HomePlayerScore();
-            var actual = target.score();
+            _target.HomePlayerScore();
+            _target.HomePlayerScore();
+            _target.HomePlayerScore();
+            var actual = _target.score();
             Assert.AreEqual("Forty Love", actual);
         }
         [TestMethod]
         public void ThirtyLove()
         {
-            var target = new TennisGame();
-            target.HomePlayerScore();
-            target.HomePlayerScore();
-            var actual = target.score();
+            _target.HomePlayerScore();
+            _target.HomePlayerScore();
+            var actual = _target.score();
             Assert.AreEqual("Thirty Love", actual);
         }
         [TestMethod]
         public void FifteenLove()
         {
-            var target = new TennisGame();
-            target.HomePlayerScore();
-            var actual = target.score();
+            _target.HomePlayerScore();
+            var actual = _target.score();
             Assert.AreEqual("Fifteen Love", actual);
         }
         [TestMethod]
         public void LoveAll()
         {
-            var target = new TennisGame();
-            var actual = target.score();
+            var actual = _target.score();
             Assert.AreEqual("Love All", actual);
         }
+        
     }
 }
