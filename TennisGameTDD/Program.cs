d312acea1649b3963170334f9eb1e939537baeb8
Chunhui <Chunhui831211@gmail.com>
2020-01-21 20:20:24 +0800

    console
    console

--- TennisGameTDD/Program.cs
+++ TennisGameTDD/Program.cs
@@ -10,6 +10,24 @@ namespace TennisGameTDD
     {
         static void Main(string[] args)
         {
+            var target = new TennisGame();
+            Console.WriteLine("請輸入Player1分數\n");
+            string home = Console.ReadLine();
+            Console.WriteLine("請輸入Player2分數\n");
+            string away = Console.ReadLine();
+            int player1Score = Convert.ToInt32(home);
+            int player2Score = Convert.ToInt32(away);
+            for (var i = 0; i < player1Score; i++)
+            {
+                target.HomePlayerScore();
+            }
+            for (var i = 0; i < player2Score; i++)
+            {
+                target.AwayPlayerScore();
+            }
+            var result = target.score();
+            Console.WriteLine("分數"+player1Score + ":"+ player2Score + "------"+result);
+            Console.ReadLine();
         }
     }
 }
